0888a685d5f793d86f34cfcaee8f031fe2628c97
wangxu <wx17629060715@163.com>
2020-08-16 17:17:51 +0800

    项目发布

---
+++ student/src/com/baizhi/entity/Student.java
@@ -0,0 +1,59 @@
+package com.baizhi.entity;
+
+public class Student {
+	private String name;
+	private String number;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((number == null) ? 0 : number.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (number == null) {
+			if (other.number != null)
+				return false;
+		} else if (!number.equals(other.number))
+			return false;
+		return true;
+	}
+	public Student(String name, String number) {
+		super();
+		this.name = name;
+		this.number = number;
+	}
+	
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	
+
+}
